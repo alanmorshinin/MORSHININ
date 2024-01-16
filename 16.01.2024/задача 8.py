db0369c1e92518508b410a6eac13340e34ac9d1e
student <fx@dfd>
2024-01-16 12:51:08 +1200

    1211

--- 16.01.2024/задача 8.py
+++ 16.01.2024/задача 8.py
@@ -1,4 +1,4 @@
-def check_apartment(a, b):
+def alan_check(a, b):
     if b >= 100 and a <= 10000000:
         return "Квартира подходит"
     elif b >= 80 and a <= 7000000:
@@ -7,5 +7,5 @@ def check_apartment(a, b):
         return "Квартира не подходит"
 a = float(input("Введите стоимость квартиры: "))
 b = float(input("Введите площадь квартиры: "))
-result = check_apartment(a, b)
+result = alan_check(a, b)
 print(result)
