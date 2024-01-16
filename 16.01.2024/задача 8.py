094ee9ef4bbedd5f06274842ea85b311c648cc95
student <fx@dfd>
2024-01-16 12:41:36 +1200

    1211

--- 16.01.2024/задача 8.py
+++ 16.01.2024/задача 8.py
@@ -1,1 +1,11 @@
-аоа
+def check_apartment(a, b):
+    if b >= 100 and a <= 10000000:
+        return "Квартира подходит"
+    elif b >= 80 and a <= 7000000:
+        return "Квартира подходит"
+    else:
+        return "Квартира не подходит"
+a = float(input("Введите стоимость квартиры: "))
+b = float(input("Введите площадь квартиры: "))
+result = check_apartment(a, b)
+print(result)
--- 16.01.2024/задача 9.py
+++ 16.01.2024/задача 9.py
@@ -1,1 +1,5 @@
-вп
+number = input("Введите трехзначное число: ")
+a = int(number[0]) + int(number[1])
+b = int(number[1]) + int(number[2])
+result = str(min(a, b)) + str(max(a, b))
+print("Искомое число:", result)
