be7d5d01028120701117684f91e48900dae9ae84
student <fx@dfd>
2024-01-16 13:10:41 +1200

    1211

--- 12.01.2024/Задача 6 дз.py
+++ 12.01.2024/Задача 6 дз.py
@@ -2,16 +2,16 @@ age_of_Anton = int(input("Возраст Антона: "))
 age_of_Boris = int(input("Возраст Бориса: "))
 age_of_Victor = int(input("Возраст Виктора: "))
 if age_of_Anton > age_of_Boris and age_of_Anton > age_of_Victor:
-print("Антон старше всех.")
+    print("Антон старше всех.")
 elif age_of_Boris > age_of_Anton and age_of_Boris > age_of_Victor:
-print("Борис старше всех.")
+    print("Борис старше всех.")
 elif age_of_Victor > age_of_Anton and age_of_Victor > age_of_Boris:
-print("Виктор старше всех.")
+    print("Виктор старше всех.")
 elif age_of_Anton == age_of_Boris and age_of_Anton > age_of_Victor:
-print("Антон и Борис старше Виктора.")
+    print("Антон и Борис старше Виктора.")
 elif age_of_Anton == age_of_Victor and age_of_Anton > age_of_Boris:
-print("Антон и Виктор старше Бориса.")
+    print("Антон и Виктор старше Бориса.")
 elif age_of_Boris == age_of_Victor and age_of_Boris > age_of_Anton:
-print("Борис и Виктор старше Антона.")
+    print("Борис и Виктор старше Антона.")
 else:
-print("Антон, Борис и Виктор одного возраста.")
+    print("Антон, Борис и Виктор одного возраста.")
