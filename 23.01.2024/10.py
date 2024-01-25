d8fb75421d340b35b0fa66f52a72a6cd0b1c7d83
student <fx@dfd>
2024-01-25 14:39:52 +1200

    1211

--- 23.01.2024/10.py
+++ 23.01.2024/10.py
@@ -1,7 +1,7 @@
 x = int(input("Введите количество мальчиков: "))
 y = int(input("Введите количество девочек: "))
 
-if x < y or x > 2 * y + 1:
+if x > (y + 1):
     print("Ответ: Нет решения")
 else:
     result = ""
@@ -9,6 +9,6 @@ else:
         result += "BG"
         x -= 1
         y -= 1
-    result += "B" * x
     result += "G" * y
+    result += "B" * x
     print("Ответ:", result)
