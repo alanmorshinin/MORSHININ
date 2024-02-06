cc34e9cc67009a2b9937ddfe26fce20f3ce4d9c0
student <fx@dfd>
2024-02-06 13:15:02 +1200

    1211

--- 06.02.2024/6.py
+++ 06.02.2024/6.py
@@ -1,8 +1,10 @@
 def line(s, t):
     k, b = map(float, s.split('x'))
-    #print(f'{k:<5}{b:<5}')
+    # print(f'{k:<5}{b:<5}')
     x, y = map(float, t.split(';'))
-    #print(f'{x:<5}{y:<}')
+    # print(f'{x:<5}{y:<}')
 
     print(k * x + b == y)
+
+
 line("1x+6", "1;7")
