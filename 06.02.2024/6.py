5b3cb38adb19043be993003f85cacfcc73093260
student <fx@dfd>
2024-02-06 13:13:56 +1200

    1211

--- 06.02.2024/6.py
+++ 06.02.2024/6.py
@@ -1,10 +1,8 @@
 def line(s, t):
-    k, b = map(float, s.split())
-    print(f'{k:<5}{b:<5}')
-    x, y = map(float, t.split())
-    print(f'{x:<5}{y:<}')
-    if k * x + b == y:
-        print('True')
-    else:
-        print('False')
+    k, b = map(float, s.split('x'))
+    #print(f'{k:<5}{b:<5}')
+    x, y = map(float, t.split(';'))
+    #print(f'{x:<5}{y:<}')
+
+    print(k * x + b == y)
 line("1x+6", "1;7")
