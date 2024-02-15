77ad2e5ce7ba78f8894aac7c5d9dbba0e91abb67
Алан Морщинин <156032277+alanmorshinin@users.noreply.github.com>
2024-02-15 20:35:25 +1200

    Update 3.py

--- 13.02.2024/3.py
+++ 13.02.2024/3.py
@@ -1,1 +1,11 @@
-
+def alan(n):
+    summ = 0
+    while n:
+        summ += n % 10
+        n //= 10
+    return summ
+ 
+a = int(input('Введите первое число: '))
+b = int(input('Введите второе число: '))
+ 
+print(('b больше', 'a больше')[alan(a) > alan(b)])
