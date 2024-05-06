2a8da65dc4346465abda0875a8d5d82826cf159f
Алан Морщинин <156032277+alanmorshinin@users.noreply.github.com>
2024-05-06 21:00:42 +1200

    C.py

--- 19 practic/C.py
+++ 19 practic/C.py
@@ -1,13 +1,18 @@
-def alan(n):
-    c = 1
-    c1 = []
-    for i in range(len(n)):
-        if n[i] == n[i+1:i+2]:
-            c += 1
-            continue
-        c1.append(n[i] + str(c))
-        c = 1
-    return c1
-n = input("")
-r = alan(n)
-print(''.join(r))
+def alan(s):
+    roman_numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0
+    prev_value = 0
+
+    for char in s:
+        value = roman_numerals.get(char, 0)
+        result += value
+        if value > prev_value:
+            result -= 2 * prev_value
+        prev_value = value
+
+    return result
+
+string = input("Enter a string: ")
+result = alan(string)
+print("Result:")
+print(result)
