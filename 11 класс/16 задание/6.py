dcd826878a3ad8d33b0a99ec99870c69cd4e219c
student <fx@dfd>
2024-12-09 12:03:17 +1200

    1211

--- 11 класс/16 задание/6.py
+++ 11 класс/16 задание/6.py
@@ -1,14 +1,13 @@
-
+c = 0
 def F(n):
     if n <= 18:
         return n + 3
-    if n % 3 == 0:
-        return F(n // 3) * F(n // 3) + n - 12
-    return F(n - 1) + n * n + 5
+    if n > 18 and n % 3 == 0:
+        return (n // 3) * F(n // 3) + n - 12
+    return F(n - 1) + n*n + 5
 
-c = 0
 
-for i in range(1, 801):
-    if all(int(j) % 2 == 0 for j in str(F(i))):
+for i in range(801):
+    if all( int(j) % 2 == 0 for j in str(F(i))):
         c += 1
 print(c)
